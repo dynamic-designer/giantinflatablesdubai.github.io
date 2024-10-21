32a9547606a8a2dea4e8ea70fe8b9b9efa41267f
dynamic-designer <w3.vimal@gmail.com>
2024-10-21 20:32:32 +0530

    mobile fixes and animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,14 +10,16 @@ $( document ).ready( function() {
 		$(this).toggleClass('is-open');
 		$('.main-dropdown-nav').slideToggle();
 	});
-	$('.main-dropdown-nav-wrap').mouseenter(function() {
-		$(this).children(".hamburger").addClass('is-open');
-		$('.main-dropdown-nav').slideDown();
-	});
-	$('.main-dropdown-nav').mouseleave(function() {
-		$(this).siblings(".hamburger").removeClass('is-open');
-		$('.main-dropdown-nav').slideUp();
-	});
+	if (window.matchMedia('(min-width: 991px)').matches) {
+		$('.main-dropdown-nav-wrap').mouseenter(function() {
+			$(this).children(".hamburger").addClass('is-open');
+			$('.main-dropdown-nav').slideDown();
+		});
+		$('.main-dropdown-nav').mouseleave(function() {
+			$(this).siblings(".hamburger").removeClass('is-open');
+			$('.main-dropdown-nav').slideUp();
+		});
+	};
 	$(document).on("click", function (event) {
 		if ($(event.target).closest(".main-dropdown-nav-wrap").length === 0) {
 			$('.main-dropdown-nav-wrap .hamburger').removeClass('is-open');
